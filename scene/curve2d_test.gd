9d3652c5baf5f0f9b9e2ea2f7153452b3911dc04
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2017-08-07 18:32:40 -0700

    Got many point Catmull-Rom Cardinal splines working.

--- scene/curve2d_test.gd
+++ scene/curve2d_test.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+# Based upon https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+
 var points = []
 var interp_amount = 10
 
@@ -8,7 +10,9 @@ func _ready():
 
 func _input(event):
 	if event.is_action_pressed("ui_click"):
-		points.append(get_global_mouse_pos())
+		# Spline can't have two overlapping points that are adjacent in order
+		if points.back() != get_global_mouse_pos():
+			points.append(get_global_mouse_pos())
 		self.update()
 	elif event.is_action_pressed("ui_left"):
 		get_node("camera").make_current()
@@ -23,60 +27,21 @@ func _input(event):
 		get_node("camera/pos").set_text(str(get_global_mouse_pos()))
 
 func _draw():
-	"""if points.size() != 0:
-		var last_point = points[0]
-		var seg = 1
-		while seg < points.size():
-			var i = 0
-			while i < interp_amount:
-				#var s = i / interp_amount
-				#var h1 = 2*s^3 - 3*s^2 + 1
-				#var h2 = -2*s^3 - 3*s^2
-				#var h3 = s^3 - 2*s^2 + s
-				#var h4 = s^3 - s^2
-				#var t1 = 0.5 * (points[seg+1]-points[seg-1])
-				#var t2 = 0.5 * (points[seg+2]-points[seg+1])
-				#var p = Vector2(h1,h1)*points[seg]+Vector2(h2, h2)*points[seg+1]+Vector2(h3,h3)*t1+Vector2(h4,h4)*t2
-				var s = float(i) / float(interp_amount)
-				var h1 = 2*pow(s,3) - 3*pow(s,2) + 1;
-				var h2 = -2*pow(s,3) - 3*pow(s,2);
-				var h3 = pow(s,3) - 2*pow(s,2) + s;
-				var h4 = pow(s,3) - pow(s,2);
-				var t1 = Vector2(0, 0)
-				var t2 = Vector2(0, 0)
-				if seg > 0 && seg+1 < points.size():
-					t1 = 0.5 * (points[seg+1]-points[seg-1])
-				if seg+2 < points.size():
-					t2 = 0.5 * (points[seg+2]-points[seg])
-				var next_seg = Vector2(50, 50)
-				if points.has(seg+1):
-					next_seg = points[seg+1]
-					print(next_seg, " foo")
-				var p = Vector2(h1,h1)*points[seg]+Vector2(h2, h2)*next_seg+Vector2(h3,h3)*t1+Vector2(h4,h4)*t2
-				draw_line(last_point, p, Color(255, 0, 0))
-				last_point = p
-				i += 1
-			seg += 1"""
 	if points.size() < 2:
 		return
 	
-	var i = 0
-	while i <= points.size():
-		var P0 = points[i]-Vector2(100, 0)
-		var P3 = points[i+1]+Vector2(100, 0)
-		if i > 0:
-			P0 = points[i-1]
-		if i+4 < points.size():
-			P3 = points[i+4]
-		var end_pnts = interpolate(P0, points[i], points[i+1], P3)
-		var last_point = points[i]
-		for pnt in end_pnts:
-			draw_line(last_point, pnt, Color(255, 255, 255))
-			last_point = pnt
-		i += 4
+	var C = []
+	for i in range(points.size()-3):
+		draw_circle(points[i], 10, Color(0, 0, 0))
+		C = interpolate(points[i], points[i+1], points[i+2], points[i+3], C, 60)
+	
+	var last_point = points.front()
+	for pnt in C:
+		draw_line(last_point, pnt, Color(255, 255, 255))
+		last_point = pnt
 
 # Interpolates the 4 points of cardinal spline
-func interpolate(P0, P1, P2, P3, interp_point_amount = 100):
+func interpolate(P0, P1, P2, P3, C_arr, interp_point_amount = 100):
 	# P[n] should be Vector2
 	
 	# calculate tangents using centripetal Catmull-Rom
@@ -84,9 +49,6 @@ func interpolate(P0, P1, P2, P3, interp_point_amount = 100):
 	var t1 = ccr(t0, P0, P1)
 	var t2 = ccr(t1, P1, P2)
 	var t3 = ccr(t2, P2, P3)
-	
-	# an array holding the endpoints of the lines
-	var end_pnts = []
 	
 	# t is the segment of interpolation we're in, based off of the interp_point_amount
 	var t = t1
@@ -99,11 +61,11 @@ func interpolate(P0, P1, P2, P3, interp_point_amount = 100):
 		var B2 = (t3-t)/(t3-t1)*A2 + (t-t1)/(t3-t1)*A3
 		
 		var C = (t2-t)/(t2-t1)*B1 + (t-t1)/(t2-t1)*B2
-		end_pnts.append(C)
+		C_arr.append(C)
 		
 		t += ((t2-t1)/interp_point_amount)
 		
-	return end_pnts
+	return C_arr
 
 # Centripetal Catmull-Rom, returns a float
 func ccr(ti, Pi, Pj, alpha = 0.5):
